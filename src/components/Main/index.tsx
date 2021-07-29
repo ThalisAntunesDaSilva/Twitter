b5c7201a3b6b556a5d73cafe498f419a3ff1527d
Thalis Antunes <thalisantunes@hotmail.com>
2021-07-29 10:14:20 -0300

    feat: Create project, implemented layout main and header of home page

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+
+        <ProfileInfo>
+          <strong>Thalis Antunes</strong>
+          <span>612 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      {/*
+<ProfilePage />
+*/}
+      {/*
+<BottomMenu>
+  <HomeIcon/>
+  <SearchIcon />
+  <BellIcon />
+  <BellIcon />
+
+</BottomMenu>
+*/}
+    </Container>
+  );
+};
+
+export default Main;
